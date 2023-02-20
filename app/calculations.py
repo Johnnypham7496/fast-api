fd6a2e6a3fc8b905e15985b701c458fb99d2637a
Johnny Pham <johnpham7496@gmail.com>
2023-02-19 20:43:08 -0600

    added insufficient funds class and function with a test function

--- app/calculations.py
+++ app/calculations.py
@@ -11,6 +11,9 @@ def divide(num1: int, num2: int):
     return num1 / num2
 
 
+class InsufficientFunds(Exception):
+    pass
+
 class BankAccount():
     def __init__(self, starting_balance=0):
         self.balance= starting_balance
@@ -18,8 +21,10 @@ class BankAccount():
     def deposit(self, ammount):
         self.balance += ammount
 
-    def withdraw(self, withdraw):
-        self.balance -= withdraw
+    def withdraw(self, amount):
+        if amount > self.balance:
+            raise InsufficientFunds("Insufficient funds in account")
+        self.balance -= amount
 
     def collect_interest(self):
         self.balance *= 1.1
