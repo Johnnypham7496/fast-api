a8f76b124ff1796026abaa8546b83a14c2bab194
Johnny Pham <johnpham7496@gmail.com>
2023-02-21 21:39:50 -0600

    added pytest fixture to create and delete tables after testing

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,23 +1,26 @@
 from fastapi.testclient import TestClient
+import pytest
 from sqlalchemy import create_engine
 from app.main import app
 from app import schemas
 from app.config import settings
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from app.database import get_db
+from app.database import get_db, Base
 
 
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://postgres:password123@localhost:5432:/fastapi_test'
-# SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
+
+# SQLALCHEMY_DATABASE_URL = f'postgresql://postgres:password123@localhost:5432:/fastapi_test'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
 
 # creating an engine and this is what sqlalchemy uses to connect with postgres
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine) 
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+Base.metadata.create_all(bind=engine)      # this will create our test database tables
+
 
 def override_get_db():
     db = TestingSessionLocal()
@@ -29,16 +32,20 @@ def override_get_db():
 app.dependency_overrides[get_db] = override_get_db
 
 
-client = TestClient(app)
+@pytest.fixture
+def client(): 
+    Base.metadata.drop_all(bind=engine)  # run our code after our test finishes
+    Base.metadata.create_all(bind=engine)  # run our code before we run our test
+    yield TestClient(app)
 
 
-def test_root():
+def test_root(client):
     res = client.get("/")
     print(res.json().get('message'))
     assert res.json().get('message') == 'Hello World'
     assert res.status_code == 200
 
-def test_create_user():
+def test_create_user(client):
     res = client.post("/users/", json={"email": "hello123@gmail.com", "password": "password123"})
     new_user = schemas.UserOut(**res.json())
     assert new_user.email == "hello123@gmail.com"
