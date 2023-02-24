4e347c11b34a65b8457428cacffc36a597d90748
Johnny Pham <johnpham7496@gmail.com>
2023-02-23 20:11:09 -0600

    added jwt token validation to login_users

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,6 +1,8 @@
 from app import schemas
 from .database import client, session
 import pytest
+from jose import jwt
+from app.config import settings
 
 
 @pytest.fixture
@@ -30,4 +32,9 @@ def test_create_user(client):
 
 def test_login_user(client, test_user):
     res = client.post("/login", data={"username": test_user['email'], "password": test_user['password']})
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(login_res.access_token, settings.secret_key, algorithms= [settings.algorithum])
+    id = payload.get("user_id")
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
     assert res.status_code == 200
