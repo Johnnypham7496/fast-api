02fd4b220f9274b38e3020ff813db07866a88aba
Johnny Pham <johnpham7496@gmail.com>
2023-02-26 17:37:38 -0600

    added parameters to incorrect login function

--- tests/test_users.py
+++ tests/test_users.py
@@ -40,7 +40,17 @@ def test_login_user(client, test_user):
     assert res.status_code == 200
 
 
-def test_incorrect_login(test_user, client):
-    res = client.post("/login", data={"username": test_user['email'], "password": "wrongPassword"})
-    assert res.status_code == 403
-    assert res.json().get('detail') == 'Invalid Credentials'
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@gmail.com', 'password123', 403),
+    ('johnny@gmail.com', 'wrongpassword', 403),
+    ('wrongemail@gmail.com', 'wrongpassword', 403),
+    (None, 'password123', 422),
+    ('johnny@gmail.com', None, 422)
+])
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post("/login", data={"username": email, "password": password})
+    assert res.status_code == status_code
+    # assert res.json().get('detail') == 'Invalid Credentials'
+
+
