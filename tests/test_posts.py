4283ccefe0038c5e42d457e054b4810764d392f6
Johnny Pham <johnpham7496@gmail.com>
2023-03-01 18:36:45 -0600

    added test for unauthorized user getting all posts

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -5,5 +5,14 @@ from app import schemas
 
 def test_get_all_posts(authorized_client, test_posts):
     res = authorized_client.get("/posts/")
+    def validate(post):
+        return schemas.PostOut(**post)
+    post_map = map(validate, res.json())
+    posts_list = list(post_map)
     assert len(res.json()) == len(test_posts)
     assert res.status_code == 200
+    
+
+def test_unauthorized_user_get_all_posts(client, test_posts):
+    res = client.get("/posts/")
+    res.status_code == 401
