d5bee19a51d935ed068acb480b3c4013e345fbe2
Johnny Pham <johnpham7496@gmail.com>
2023-03-01 18:45:45 -0600

    added more testing for individual posts

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -15,4 +15,22 @@ def test_get_all_posts(authorized_client, test_posts):
 
 def test_unauthorized_user_get_all_posts(client, test_posts):
     res = client.get("/posts/")
-    res.status_code == 401
+    assert res.status_code == 401
+
+
+def test_unauthorized_user_get_one_posts(client, test_posts):
+    res = client.get(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+
+def test_get_one_posts_not_exist(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/88888")
+    assert res.status_code == 404
+
+
+def test_get_one_post(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/{test_posts[0].id}")
+    post = schemas.PostOut(**res.json())
+    assert post.Post.id == test_posts[0].id
+    assert post.Post.content == test_posts[0].content
+    assert post.Post.title == test_posts[0].title
