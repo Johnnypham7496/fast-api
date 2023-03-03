b3c3efac0e3dcc7797c9c06f56cf1a38b4b329f6
Johnny Pham <johnpham7496@gmail.com>
2023-03-02 19:29:09 -0600

    added test function for updating post

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -84,3 +84,16 @@ def test_delete_post_non_exist(authorized_client, test_user, test_posts):
 def test_delete_other_user_post(authorized_client, test_user, test_posts):
     res = res = authorized_client.delete(f"/posts/{test_posts[3].id}")
     assert res.status_code == 403
+
+
+def test_update_post(authorized_client, test_posts, test_user):
+    data = {
+        "title": "updated title",
+        "content": "updated content",
+        "id": test_posts[0].id
+    }
+    res = authorized_client.put(f"/posts/{test_posts[0].id}", json=data)
+    updated_post = schemas.Post(**res.json())
+    assert res.status_code == 200
+    assert updated_post.title == data['title']
+    assert updated_post.content == data['content']
